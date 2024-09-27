e1b6b750e0dc896d0a26dd8a9ed9d60a6673762a
M.V <marco.mov98@gmail.com>
2024-09-27 10:07:24 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,5 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let who = ["El perro", "El vecino", "El Gabriel", "Un moro"];
+  let action = ["Se comió", "Me pegó", "Me insultó", "Me rompió"];
+  let what = ["Mi comida", "mi movil", "mis deberes", "mi Ps5"];
+  let when = [
+    "Ayer por la mañana",
+    "Ayer por la noche",
+    "en 5 años",
+    "Cuando me case"
+  ];
+
+  let variable1 = Math.floor(Math.random() * who.length);
+  let variable2 = Math.floor(Math.random() * action.length);
+  let variable3 = Math.floor(Math.random() * what.length);
+  let variable4 = Math.floor(Math.random() * when.length);
+
+  document.getElementById("#excuse").innerHTML =
+    who[variable1] +
+    " " +
+    action[variable2] +
+    " " +
+    what[variable3] +
+    " " +
+    when[variable4];
   console.log("Hello Rigo from the console!");
 };
